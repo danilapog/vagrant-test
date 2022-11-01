c6ed9fe979b8731765d42d4d3736fb0cc41cf97b
Danil Titarenko <77471369+danilapog@users.noreply.github.com>
2022-11-02 01:00:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ healthcheck_services () {
 }
 
 install_workspace () {
-
-   apt-get remove postfix -y 
+   if [ ! -f /etc/centos-release ]; then apt-get remove postfix -y ; fi
+   echo '127.0.0.1 host4test' | sudo tee -a /etc/hosts
    wget https://download.onlyoffice.com/install/workspace-install.sh 
    echo "N" | sudo bash workspace-install.sh --skiphardwarecheck true --makeswap false
    if [ $? != 0 ] ; then echo "exit with 0" ; exit 0; fi
