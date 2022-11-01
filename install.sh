d126594f1aad11b3c1b9bfe07625b122c57f760d
Danil Titarenko <77471369+danilapog@users.noreply.github.com>
2022-11-02 00:51:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-apt-get remove postfix -y 
-wget https://download.onlyoffice.com/install/workspace-install.sh 
-echo "N" | sudo bash workspace-install.sh --skiphardwarecheck true --makeswap false
-if [ $? != 0 ] ; then exit 1; else exit 0; fi
+prepare_debian () {
+
+}
+
+healthcheck_services () {
+
+}
+
+install_workspace () {
+
+   apt-get remove postfix -y 
+   wget https://download.onlyoffice.com/install/workspace-install.sh 
+   echo "N" | sudo bash workspace-install.sh --skiphardwarecheck true --makeswap false
+   if [ $? != 0 ] ; then exit 1; else healthcheck_services; fi
+
+}
+
