b33bb4ed36ae4d10bcee21cceffa6a8cfca1b2b0
Danil Titarenko <77471369+danilapog@users.noreply.github.com>
2022-11-02 00:54:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,8 @@ install_workspace () {
    apt-get remove postfix -y 
    wget https://download.onlyoffice.com/install/workspace-install.sh 
    echo "N" | sudo bash workspace-install.sh --skiphardwarecheck true --makeswap false
-   if [ $? != 0 ] ; then exit 1; else healthcheck_services; fi
+   if [ $? != 0 ] ; then echo "exit with 0" ; exit 0; fi
 
 }
 
+install_workspace
