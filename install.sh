5f9f555ad38510d80d7dc6fe716b5a72d8a9f44d
danilapog <danil.titarenko@onlyoffice.com>
2022-11-08 19:16:51 +0300

    Refactoring

--- install.sh
+++ install.sh
@@ -27,36 +27,35 @@ SERVICES_SYSTEMD=(
 
 prepare_vm() {
   if [ ! -f /etc/centos-release ]; then 
-     apt-get remove postfix -y 
-     echo "PREPAVE_VM: Postfix was removed <=="
+    apt-get remove postfix -y 
   fi
 
-  echo '127.0.0.1 host4test' | sudo tee -a /etc/hosts         
-  echo "PREPAVE_VM: Hostname was setting up"   
+  echo '127.0.0.1 host4test' | sudo tee -a /etc/hosts 
+  echo "PREPAVE_VM: Postfix was removed <<=="  
+  echo "PREPAVE_VM: Hostname was setting up <<=="   
 
 }
 
 install_workspace() {
-
   wget https://download.onlyoffice.com/install/workspace-install.sh 
   echo "N" | bash workspace-install.sh --skiphardwarecheck true --makeswap false 
-
 }
 
 healthcheck_services() {
-   for service in ${SERVICES_SYSTEMD[@]} 
-     do 
-     if systemctl is-active --quiet ${service}; then
-         echo "OK: Service ${service} is running"
-     else 
-         echo "FAILED: Service ${service} is not running"
-     fi
-   done
+  for service in ${SERVICES_SYSTEMD[@]} 
+    do 
+      if systemctl is-active --quiet ${service}; then
+        echo "OK: Service ${service} is running"
+      else 
+        echo "FAILED: Service ${service} is not running"
+        SERVICES_FAILED="true"
+      fi
+  done
 
-   if [ ! -z "${SERVICES_FAILED}" ]; then
-          echo "Some sevices is not running"
-          exit 1
-   fi
+  if [ ! -z "${SERVICES_FAILED}" ]; then
+    echo "Some sevices is not running"
+    exit 1
+  fi
 }
 
 
